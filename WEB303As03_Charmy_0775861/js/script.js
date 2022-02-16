6dc3e6a2654a824c11be2768021a7013fe12a1c6
CharmyyPatel <90396296+CharmyyPatel@users.noreply.github.com>
2022-02-16 11:45:49 -0500

    Update script.js

--- WEB303As03_Charmy_0775861/js/script.js
+++ WEB303As03_Charmy_0775861/js/script.js
@@ -24,3 +24,42 @@ $.ajax('team.json',
         $('p').append('Loading... ' + errorMessage);
     }
 });
+
+
+// Solution provided by Sir during Lab Time..........................................
+
+$(function () {
+    getJSONfunction();
+    AJAXfunction();
+});
+
+function getJSONfunction() {
+    $.getJSON("team.json", function (data) {
+        processdata();
+    });
+}
+
+function AJAXfunction() {
+    $.ajax({
+        url: "team.json",
+        method: "GET",
+
+        beforeSend: function () {
+            $('div#team').text("Loading...");
+        }
+    }).done(function (data) {
+        setTimeout(() => processdata(data), 3000);
+    }).fail(function () {
+        $('div#team').text("Could not get the Data");
+    });
+}
+
+function processsdata(data) {
+    $.each(data.members, function (key, val) {
+        console.log("data key: ", key, " and data value: ", val);
+
+        $('div#team').append("<h2> + val.name + </h2>");
+        $('div#team').append("<h5> + val.position + </h5>");
+        $('div#team').append("<p> + val.bio + </p>");
+    });
+}
